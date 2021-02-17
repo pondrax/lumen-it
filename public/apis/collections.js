90e8848afb2e81d0f0b838e5978067f7f1242fbf
pondrax <pondras.setiawan@gmail.com>
2021-02-17 14:56:26 +0700

    Clean api test

--- public/apis/collections.js
+++ public/apis/collections.js
@@ -1,4 +1,5 @@
 const collections = [
+	//Auth
 	{
 		method :'POST',
 		url : 'api/auth/login',
@@ -16,28 +17,109 @@ const collections = [
 			password : 'lumen123',
 		}
 	},
+	
+	// Role
 	{
 		method :'GET',
-		url : 'api/app/role/all?limit=5&page=1&sort=id&order=desc',
+		url : 'api/app/role/read?search=&limit=5&page=1&sort=id&order=desc',
 	},
 	{
-		method :'GET',
-		url : 'api/app/user/all?limit=5&page=1&sort=id&order=desc',
+		method :'POST',
+		url : 'api/app/role',
+		body : {
+			role : 'Role name',
+			description	 : 'Role description',
+		}
 	},
 	{
-		method :'GET',
-		url : 'api/app/menu/all?limit=5&page=1&sort=id&order=desc',
+		method :'PUT',
+		url : 'api/app/role/5',
+		body : {
+			role : 'Role name edit',
+			description	 : 'Role description edit',
+		}
 	},
 	{
-		method :'GET',
-		url : 'api/app/route/all?limit=5&page=1&sort=id&order=desc',
+		method :'DELETE',
+		url : 'api/app/role/5'
 	},
+	
+	// User
 	{
 		method :'GET',
-		url : 'api/app/access/all?limit=5&page=1&sort=id&order=desc',
+		url : 'api/app/user/read?search=&limit=5&page=1&sort=id&order=desc',
 	},
 	{
+		method :'POST',
+		url : 'api/app/user',
+		body : {
+			username : 'user name',
+			email : 'user@email.com',
+			password : '12345678',
+		}
+	},
+	{
+		method :'PUT',
+		url : 'api/app/user/5',
+		body : {
+			user : 'user name edit',
+			description	 : 'user description edit',
+		}
+	},
+	{
+		method :'DELETE',
+		url : 'api/app/user/5'
+	},
+	
+	// Menu
+	{
 		method :'GET',
-		url : 'api/app/logs/all?limit=5&page=1&sort=id&order=desc',
-	}
+		url : 'api/app/menu/read?search=&limit=5&page=1&sort=id&order=desc',
+	},
+	{
+		method :'POST',
+		url : 'api/app/menu',
+		body : {
+			menu : 'menu name',
+			description	 : 'menu description',
+		}
+	},
+	{
+		method :'PUT',
+		url : 'api/app/menu/5',
+		body : {
+			menu : 'menu name edit',
+			description	 : 'menu description edit',
+		}
+	},
+	{
+		method :'DELETE',
+		url : 'api/app/menu/5'
+	},
+	
+	// Route
+	{
+		method :'GET',
+		url : 'api/app/route/read?search=&limit=5&page=1&sort=id&order=desc',
+	},
+	{
+		method :'POST',
+		url : 'api/app/route',
+		body : {
+			route : 'route name',
+			description	 : 'route description',
+		}
+	},
+	{
+		method :'PUT',
+		url : 'api/app/route/5',
+		body : {
+			route : 'route name edit',
+			description	 : 'route description edit',
+		}
+	},
+	{
+		method :'DELETE',
+		url : 'api/app/route/5'
+	},
 ];
